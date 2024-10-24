66cae3fade4f8982743c48db6d1e68a4a565d9ae
Charuka Deshan <itscharukadeshan@gmail.com>
2024-10-24 15:39:29 +0530

    feat✨: Update the archive func to easily readable

--- src/services/archive.ts
+++ src/services/archive.ts
@@ -5,39 +5,32 @@ import ada from "../scrapers/ada";
 import adaderana from "../scrapers/adaderana";
 import dailyMirror from "../scrapers/dailyMirror";
 import economyNext from "../scrapers/economynext";
-import scrapeIsland from "../scrapers/island";
+import island from "../scrapers/island";
 import lankadeepa from "../scrapers/lankadeepa";
 import tamilMirror from "../scrapers/tamilMirror";
 import theMorning from "../scrapers/theMorning";
 import thinakaran from "../scrapers/thinakaran";
 
 export async function archive(url: string) {
-  if (
-    url === Urls.ADADERANA_ENGLISH ||
-    url === Urls.ADADERANA_SINHALA ||
-    url === Urls.ADADERANA_TAMIL
-  ) {
-    return await adaderana(url);
-  } else if (
-    url === Urls.ARUNA ||
-    url === Urls.THE_MORNING ||
-    url === Urls.THAMILAN
-  ) {
-    return await theMorning(url);
-  } else if (url === Urls.DAILY_MIRROR) {
-    return await dailyMirror(url);
-  } else if (url === Urls.TAMIL_MIRROR) {
-    return await tamilMirror(url);
-  } else if (url === Urls.ISLAND) {
-    return await scrapeIsland(url);
-  } else if (url === Urls.LANKADEEPA) {
-    return await lankadeepa(url);
-  } else if (url === Urls.ECONOMY_NEXT) {
-    return await economyNext(url);
-  } else if (url === Urls.ADA) {
-    return await ada(url);
-  } else if (url === Urls.THINAKARAN || url === Urls.DINAMINA) {
-    return await thinakaran(url);
+  const urlHandlers: { [key: string]: () => Promise<any> } = {
+    [Urls.ADADERANA_ENGLISH]: () => adaderana(url),
+    [Urls.ADADERANA_SINHALA]: () => adaderana(url),
+    [Urls.ADADERANA_TAMIL]: () => adaderana(url),
+    [Urls.ARUNA]: () => theMorning(url),
+    [Urls.THE_MORNING]: () => theMorning(url),
+    [Urls.THAMILAN]: () => theMorning(url),
+    [Urls.DAILY_MIRROR]: () => dailyMirror(url),
+    [Urls.TAMIL_MIRROR]: () => tamilMirror(url),
+    [Urls.ISLAND]: () => island(url),
+    [Urls.LANKADEEPA]: () => lankadeepa(url),
+    [Urls.ECONOMY_NEXT]: () => economyNext(url),
+    [Urls.ADA]: () => ada(url),
+    [Urls.THINAKARAN]: () => thinakaran(url),
+    [Urls.DINAMINA]: () => thinakaran(url),
+  };
+
+  if (urlHandlers[url]) {
+    return await urlHandlers[url]();
   } else {
     throw new Error(
       "Invalid URL: check the docs for valid URLs and try again."
